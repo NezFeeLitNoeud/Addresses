c58a06aeb91c47d4bd8badea907fdc8439c50696
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2020-03-02 15:37:56 +0100

    Add component and tailwind config

--- frontApp/src/app/app.module.ts
+++ frontApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAddressComponent } from './components/add-address/add-address.component';
+import { ShowAddressComponent } from './components/show-address/show-address.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAddressComponent,
+    ShowAddressComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontApp/src/app/components/show-address/show-address.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-address',
+  templateUrl: './show-address.component.html',
+  styleUrls: ['./show-address.component.css']
+})
+export class ShowAddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
